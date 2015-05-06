2d62489379504b575fe8d0d94f6e754efba4c1c6
zhan wang <zhanzw@gmail.com>
2015-05-06 20:55:15 +0100

    set up balanceTol funciton to get the total of two accounts

--- atm-start_jQuery/atm.js
+++ atm-start_jQuery/atm.js
@@ -19,15 +19,15 @@ var amtBalance1 = "0";
 
 var addCurrent = function(){
 	amtBalance1 = Number($('#amount1').val()) + Number(amtBalance1);
-	
-	console.log(amtBalance1);
+	amtEntered1 = Number($('#amount1').val());
+	console.log(amtEntered1);
 	balanceCurrent();
 };
 
 var subtractCurrent = function(){
 	amtBalance1 = Number(amtBalance1) - Number($('#amount1').val());
-	
-	console.log(amtBalance1);
+	amtEntered1 = Number($('#amount1').val());
+	console.log(amtEntered1);
 	balanceCurrent();
 };
 
@@ -36,8 +36,47 @@ var balanceCurrent = function(){
 	$('#balance1').html("$" + amtBalance1);
 };
 
-// CurrentAcct if click deposit => balance1 + amount1 | else if click withdraw =>balance1 - amount1
 
+// saving account
+
+var amtEntered2 = "0";
+var amtBalance2 = "0";
+
+var addSave = function() {
+	amtBalance2 = Number($('#amount2').val()) + Number(amtBalance2);
+	amtEntered2 = Number($('#amount2').val());
+	console.log(amtEntered2);
+	balanceSave();
+};
+
+var subtractSave = function(){
+	amtBalance2 = Number(amtBalance2) - Number($('#amount2').val());
+	amtEntered2 = Number($('#amount2').val());
+	console.log(amtEntered2);
+	balanceSave();
+};
+
+var balanceSave = function(){
+	$('#balance2').html("$" + amtBalance2);
+};
+
+//transfer between 2 account
+
+
+var balanceTol = function () {
+ return Number(amtBalance1) + Number(amtBalance2);
+
+};
+
+
+// if amtEntered1 < balanceTol => amtBalance1 = balanceTol - amtEntered1 AND amtBalance2 = amtBalance1 - amtEntered1 + amtBalance2
+if (amtEntered1 < balanceTol()) {
+	amtBalance1 = balanceTol() - amtBalance1;
+	amtBalance2 = amtBalance1 - amtBalance1 + amtBalance2;
+};
+
+
+// event listener
 $(document).ready(function(){
   $('#deposit1').on('click', addCurrent)
   $('#withdraw1').on('click', subtractCurrent)
