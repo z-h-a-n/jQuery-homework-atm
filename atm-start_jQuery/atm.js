e54340e99664a585c7edd0ce60084cfa605210a7
zhan wang <zhanzw@gmail.com>
2015-05-07 10:14:03 +0100

    attempted to connect the two accounts

--- atm-start_jQuery/atm.js
+++ atm-start_jQuery/atm.js
@@ -1,10 +1,12 @@
 // current account
+
+$(document).ready(function(){
 var amtEntered1 = "0";
 var amtBalance1 = "0";
 
 var addCurrent = function(){
 	amtBalance1 = Number($('#amount1').val()) + Number(amtBalance1);
-	amtBalance1 = Number(amtBalance1) < 0 ? 0 : Number(amtBalance1);
+	// amtBalance1 = Number(amtBalance1) < 0 ? 0 : Number(amtBalance1);
 	amtEntered1 = Number($('#amount1').val());
 	console.log(amtBalance1);
 	if (amtBalance1 >= 0) {
@@ -14,11 +16,13 @@ var addCurrent = function(){
 
 var subtractCurrent = function(){
 	if (balanceTol() > Number($('#amount1').val()) && Number(amtBalance1) < Number($('#amount1').val())) {
-		amtBalance1 = Number(amtBalance1) - Number($('#amount1').val()) + Number(amtBalance2);
+		// amtBalance1 = Number(amtBalance1) - Number($('#amount1').val()) + Number(amtBalance2);
+		amtBalance1 = "0";
+		// amtBalance2 = Number(amtBalance2) - Number($('#amount1').val())
 	} else {
 		amtBalance1 = Number(amtBalance1) - Number($('#amount1').val());
 	};
-	amtBalance1 = Number(amtBalance1) < 0 ? 0 : Number(amtBalance1);
+	// amtBalance1 = Number(amtBalance1) < 0 ? 0 : Number(amtBalance1);
 	amtEntered1 = Number($('#amount1').val());
 	console.log(amtBalance1);
 	if (amtBalance1 >= 0) {
@@ -30,6 +34,12 @@ var balanceCurrent = function(){
 $('#balance1').html("$" + amtBalance1);
 };
 
+
+
+var bal2toBal1 = function(){
+	amtBalance2 = Number(amtBalance1) - Number($('#amount1').val()) + Number(amtBalance2);
+	balanceCurrent();
+}
 
 
 // saving account
@@ -48,7 +58,11 @@ var addSave = function() {
 };
 
 var subtractSave = function(){
+	if (balanceTol() > Number($('#amount1').val()) && Number(amtBalance1) < Number($('#amount1').val())) {
+		bal2toBal1();
+	} else {
 	amtBalance2 = Number(amtBalance2) - Number($('#amount2').val());
+	}
 	amtBalance2 = Number(amtBalance2) < 0 ? 0 : Number(amtBalance2);
 	amtEntered2 = Number($('#amount2').val());
 	console.log(amtBalance2);
@@ -80,13 +94,13 @@ if (balanceTol() < Number($('#amount1').val())) {
 
 
 // event listener
-$(document).ready(function(){
+
   $('#deposit1').on('click', addCurrent)
   $('#withdraw1').on('click', subtractCurrent)
   $('#amount1').on('change')
  	$('#balance1').on('change', balanceCurrent)  
 
- 	 $('#deposit2').on('click', addSave)
+ 	$('#deposit2').on('click', addSave)
   $('#withdraw2').on('click', subtractSave)
   $('#amount2').on('change')
  	$('#balance2').on('change', balanceSave)  
